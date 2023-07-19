045a4495c92dd5f7fd6e9558c18d435b6bc68e37
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-07-19 11:51:25 -0400

    changed styles to css module

---
+++ app/components/home/card/Card.jsx
@@ -0,0 +1,27 @@
+import styles from './page.module.css';
+import Image from 'next/image';
+
+const Card = ({
+  card,
+  title,
+  blurb,
+  img,
+  alt,
+  width,
+  height,
+  cardTitle,
+  cardBlurb,
+  titleColor,
+}) => {
+  return (
+    <div className={card}>
+      <Image src={img} alt={alt} width={width} height={height} />
+      <div className={styles.text}>
+        <span className={`${cardTitle} ${titleColor}`}>{title}</span>
+        <span className={cardBlurb}>{blurb}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
