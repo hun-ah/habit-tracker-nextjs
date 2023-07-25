4ffa95c2bc1aef2cf91991c0a8605eb3376cf986
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-07-25 11:48:39 -0400

    added logic for already registered user

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -5,18 +5,22 @@ import { NextResponse } from 'next/server';
 
 export const POST = async (request) => {
   const { name, email, password } = await request.json();
-
   await connect();
-
-  const hashedPassword = await bcrypt.hash(password, 5);
-
-  const newUser = new User({
-    name,
-    email,
-    password: hashedPassword,
-  });
-
   try {
+    // Check if a user with the given email already exists
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      return new NextResponse('Email is already registered', {
+        status: 409,
+      });
+    }
+    // If no existing user found, create a new user
+    const hashedPassword = await bcrypt.hash(password, 5);
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
     await newUser.save();
     return new NextResponse('User has been created', {
       status: 201,
@@ -26,4 +30,22 @@ export const POST = async (request) => {
       status: 500,
     });
   }
+  // const { name, email, password } = await request.json();
+  // await connect();
+  // const hashedPassword = await bcrypt.hash(password, 5);
+  // const newUser = new User({
+  //   name,
+  //   email,
+  //   password: hashedPassword,
+  // });
+  // try {
+  //   await newUser.save();
+  //   return new NextResponse('User has been created', {
+  //     status: 201,
+  //   });
+  // } catch (err) {
+  //   return new NextResponse(err.message, {
+  //     status: 500,
+  //   });
+  // }
 };
