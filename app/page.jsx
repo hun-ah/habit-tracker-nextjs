c5c6baa5990e9e6c27e5b15763b9812cd1157b37
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-07-27 15:34:52 -0400

    updated openGraph image

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,10 @@ const HomePage = () => {
   return (
     <div>
       <Head>
-        <meta property='og:image' content='https://ibb.co/Yp9M2Xm' />
+        <meta
+          property='og:image'
+          content='https://i.ibb.co/3BXq1v2/255960174-a73e6aeb-bf7b-492b-9c2d-d5ed174ce423.png'
+        />
       </Head>
       <div className='flex-column'>
         <div className='inner-container'>
