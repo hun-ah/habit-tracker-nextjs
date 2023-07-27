65a001199cfd7c42975641a3675e9863cccfab6e
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-07-27 15:38:44 -0400

    removed head

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import Main from './components/home/main/Main';
 import Who from './components/home/who/Who';
 import Why from './components/home/why/Why';
@@ -6,12 +5,6 @@ import Why from './components/home/why/Why';
 const HomePage = () => {
   return (
     <div>
-      <Head>
-        <meta
-          property='og:image'
-          content='https://i.ibb.co/3BXq1v2/255960174-a73e6aeb-bf7b-492b-9c2d-d5ed174ce423.png'
-        />
-      </Head>
       <div className='flex-column'>
         <div className='inner-container'>
           <Main />
