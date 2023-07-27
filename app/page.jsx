932789ee06347334dacd4b077245190db64f037a
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-07-27 15:27:35 -0400

    added head and opengraph image

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,24 @@
+import Head from 'next/head';
 import Main from './components/home/main/Main';
 import Who from './components/home/who/Who';
 import Why from './components/home/why/Why';
 
 const HomePage = () => {
   return (
-    <div className='flex-column'>
-      <div className='inner-container'>
-        <Main />
+    <>
+      <Head>
+        <meta property='og:image' content='https://ibb.co/Yp9M2Xm' />
+      </Head>
+      <div className='flex-column'>
+        <div className='inner-container'>
+          <Main />
+        </div>
+        <Who />
+        <div className='inner-container'>
+          <Why />
+        </div>
       </div>
-      <Who />
-      <div className='inner-container'>
-        <Why />
-      </div>
-    </div>
+    </>
   );
 };
 
